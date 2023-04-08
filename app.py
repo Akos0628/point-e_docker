502ab328726bc09ea0648b149784731a0ea13297
Akos0628 <75949273+Akos0628@users.noreply.github.com>
2023-04-08 17:59:32 +0200

    the container will load the models on startup

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from point_e.util.plotting import plot_point_cloud
 from point_e.util.point_cloud import PointCloud
 
 from flask import Flask, request, jsonify, send_file
+import warnings
 
 app = Flask(__name__)
 
@@ -24,50 +25,12 @@ def generate_object():
     request_data = request.get_json()
     prompt = request_data['prompt']
 
-    print(torch.cuda.is_available())
-    print('creating base model...')
-    base_name = 'base40M-textvec'
-    base_model = model_from_config(MODEL_CONFIGS[base_name], device)
-    base_model.eval()
-    base_diffusion = diffusion_from_config(DIFFUSION_CONFIGS[base_name])
-
-    print('creating upsample model...')
-
-    print('creating upsample model...')
-    upsampler_model = model_from_config(MODEL_CONFIGS['upsample'], device)
-    upsampler_model.eval()
-    upsampler_diffusion = diffusion_from_config(DIFFUSION_CONFIGS['upsample'])
-
-    print('downloading base checkpoint...')
-    base_model.load_state_dict(load_checkpoint(base_name, device))
-
-    print('downloading upsampler checkpoint...')
-    upsampler_model.load_state_dict(load_checkpoint('upsample', device))
-
-    sampler = PointCloudSampler(
-        device=device,
-        models=[base_model, upsampler_model],
-        diffusions=[base_diffusion, upsampler_diffusion],
-        num_points=[1024, 4096 - 1024],
-        aux_channels=['R', 'G', 'B'],
-        guidance_scale=[3.0, 0.0],
-        model_kwargs_key_filter=('texts', ''), # Do not condition the upsampler at all
-    )
-
     # Produce a sample from the model.
     samples = None
     for x in tqdm(sampler.sample_batch_progressive(batch_size=1, model_kwargs=dict(texts=[prompt]))):
         samples = x
 
     pc = sampler.output_to_point_clouds(samples)[0]
-
-    print('creating SDF model...')
-    name = 'sdf'
-    model = model_from_config(MODEL_CONFIGS[name], device)
-    model.eval()
-
-    print('loading SDF model...')
-    model.load_state_dict(load_checkpoint(name, device))
 
     # Produce a mesh (with vertex colors)
     mesh = marching_cubes_mesh(
@@ -90,6 +53,50 @@ def generate_object():
 @app.route('/test', methods=['GET'])
 def load_last():
     return send_file('mesh.ply')
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if not torch.cuda.is_available():
+    warnings.warn('no GPU is available')
+
+
+print()
+print('creating base model...')
+base_name = 'base40M-textvec'
+base_model = model_from_config(MODEL_CONFIGS[base_name], device)
+base_model.eval()
+base_diffusion = diffusion_from_config(DIFFUSION_CONFIGS[base_name])
+
+print('creating upsample model...')
+
+print('creating upsample model...')
+upsampler_model = model_from_config(MODEL_CONFIGS['upsample'], device)
+upsampler_model.eval()
+upsampler_diffusion = diffusion_from_config(DIFFUSION_CONFIGS['upsample'])
+
+print('downloading base checkpoint...')
+base_model.load_state_dict(load_checkpoint(base_name, device))
+
+print('downloading upsampler checkpoint...')
+upsampler_model.load_state_dict(load_checkpoint('upsample', device))
+
+sampler = PointCloudSampler(
+    device=device,
+    models=[base_model, upsampler_model],
+    diffusions=[base_diffusion, upsampler_diffusion],
+    num_points=[1024, 4096 - 1024],
+    aux_channels=['R', 'G', 'B'],
+    guidance_scale=[3.0, 0.0],
+    model_kwargs_key_filter=('texts', ''), # Do not condition the upsampler at all
+)
+
+print('creating SDF model...')
+name = 'sdf'
+model = model_from_config(MODEL_CONFIGS[name], device)
+model.eval()
+
+print('loading SDF model...')
+model.load_state_dict(load_checkpoint(name, device))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
